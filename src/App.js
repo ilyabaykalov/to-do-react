2189143bf78c37eb4207cca67ca06ceb84f44299
Ilya Baykalov <enot_boris@icloud.com>
2020-04-17 21:09:20 +0700

    Сделана база Сделан компонент списка

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './components/List/List';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='todo'>
+			<div className='todo__sidebar'>
+				<List items={[{
+					icon: { name: 'list', color: '#7C7C7C' },
+					name: 'Все задачи'
+				}]}/>
+			</div>
+			<div className='todo__tasks'>
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
