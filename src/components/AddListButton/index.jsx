a9b2df6abd9b3a44b2c36f3a740f210e4a71ae06
Ilya Baykalov <enot_boris@icloud.com>
2020-04-19 10:24:38 +0700

    Добавлена кнопка закрытия popup меню

--- src/components/AddListButton/index.jsx
+++ src/components/AddListButton/index.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import List from '../List';
 import Badge from '../Badge';
 
 import './AddListButton.scss';
 
+library.add(fas);
+
 const AddListButton = ({ colors }) => {
 	const [isVisiblePopup, setVisibilityPopup] = useState(true);
 	const [selectedColor, selectColor] = useState(colors[0].id);
 
-	console.log(selectedColor);
 	return (
 		<div className='add-list'>
 			<List onClick={ () => setVisibilityPopup(true) }
@@ -22,10 +27,14 @@ const AddListButton = ({ colors }) => {
 			      ] }/>
 			{ isVisiblePopup && (
 				<div className='add-list__popup'>
+					<FontAwesomeIcon className={ 'add-list__popup__close-button' }
+					                 icon={ 'times-circle' }
+					                 color={ '#7F7E7E' }
+					                 onClick={ () => setVisibilityPopup(false) }/>
 					<input className='field'
 					       type='text'
 					       placeholder='Название списка'/>
-					<div className='add-list__popup-colors'>
+					<div className='add-list__popup__colors'>
 						{ colors.map(color => (
 							<Badge key={ color.id }
 							       className={ selectedColor === color.id && 'active' }
